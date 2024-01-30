e62aae1d01b99282e98d3bc575ba0bb898ba2b3d
wutongwutong00 <wu-tong-apple@hotmail.com>
2024-01-30 15:02:49 -0500

    huey.html

--- ShowOff/script.js
+++ ShowOff/script.js
@@ -3,10 +3,18 @@ document.getElementById("expand1").addEventListener("click", (e) => {
 });
 document.getElementById("expand2").addEventListener("click", (e) => {
     document.getElementById("intro2").classList.toggle("show");
+    document.getElementById("expand2").classList.toggle("names");
+    document.getElementById("expand3").classList.toggle("names");
+    document.getElementById("wavy").classList.toggle("names");
 });
 document.getElementById("expand3").addEventListener("click", (e) => {
     document.getElementById("intro3").classList.toggle("show");
 });
 document.getElementById("expand4").addEventListener("click", (e) => {
-    window.open("https:///wutongwutong00.github.io/nextLevelCode/ShowOff/huey.html", "hueywindow", "width=400,height=400");
+    document.getElementById("huey").classList.toggle("show");
+    document.getElementById("huey1").classList.toggle("show");
+});
+
+document.getElementById("huey").addEventListener("mousedown", (e) => {
+    window.open("https://wutongwutong00.github.io/nextLevelCode/ShowOff/huey.html");
 });
